cf2afbe3e68b13c82772e5b256998450409a732f
Atif Afsar <atifafsar70@gmail.com>
2025-12-13 12:50:05 +0530

    Minimise the images size

--- src/App.jsx
+++ src/App.jsx
@@ -9,12 +9,14 @@ import Contact from "./Pages/Contact";
 import Loader from "./Components/Loader";
 import SmoothScroll from "./Pages/SmoothScroll";
 import Footer from "./Components/Home/Footer";
-import Blogs from "./Pages/Blogs";
-import Blog from "./Pages/Blog";
 import ScrollToTop from "./Components/ScrollToTop";
 import { HeadProvider } from "react-head";
 import WhatsAppButton from "./Components/WhatsAppButton";
 
+import { Suspense, lazy } from "react";
+
+const Blogs = lazy(() => import("./Pages/Blogs"));
+const Blog = lazy(() => import("./Pages/Blog"));
 
 const App = () => {
   const [loading, setLoading] = useState(true);
@@ -25,30 +27,59 @@ const App = () => {
   }, []);
 
   return (
-      <HeadProvider>
-    <Router>
-      <SmoothScroll />
-      <ScrollToTop />
+    <HeadProvider>
+      <Router>
+        <SmoothScroll />
+        <ScrollToTop />
 
-      {loading ? (
-        <Loader setLoading={setLoading} />
-      ) : (
-        <>
-          <Navbar />
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/courses" element={<Courses />} />
-            <Route path="/results" element={<Result />} />
-            <Route path="/contact" element={<Contact />} />
-            <Route path="/blogs" element={<Blogs />} />
-            <Route path="/blog/:id" element={<Blog />} />
-          </Routes>
-         <WhatsAppButton />
-         <Footer />
-        </>
-      )}
-    </Router>
+        {loading ? (
+          <Loader setLoading={setLoading} />
+        ) : (
+          <>
+            <Navbar />
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/about" element={<About />} />
+              <Route path="/courses" element={<Courses />} />
+              <Route path="/results" element={<Result />} />
+              <Route path="/contact" element={<Contact />} />
+              <Route path="/blogs" element={<Blogs />} />
+              {/* Lazy Loaded Routes */}
+              <Route
+                path="/blogs"
+                element={
+                  <Suspense
+                    fallback={
+                      <div className="min-h-[60vh] flex items-center justify-center text-gray-500">
+                        Loading blogs...
+                      </div>
+                    }
+                  >
+                    <Blogs />
+                  </Suspense>
+                }
+              />
+
+              <Route
+                path="/blog/:id"
+                element={
+                  <Suspense
+                    fallback={
+                      <div className="min-h-[60vh] flex items-center justify-center text-gray-500">
+                        Loading article...
+                      </div>
+                    }
+                  >
+                    <Blog />
+                  </Suspense>
+                }
+              />
+            </Routes>
+            <WhatsAppButton />
+            <Footer />
+          </>
+        )}
+      </Router>
     </HeadProvider>
   );
 };
